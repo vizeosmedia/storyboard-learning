204dc1afc77b5cf92d58922bddf9fd5777d77968
vizeosmedia <159869493+vizeosmedia@users.noreply.github.com>
2024-03-17 18:00:48 -0700

    Added SwiperJS call

--- JS Library/contact-form-apply.js
+++ JS Library/contact-form-apply.js
@@ -17,3 +17,19 @@ document.getElementById('contact-form').addEventListener('submit', function(even
       alert(JSON.stringify(err));
     });
 });
+
+// SWIPER JS INIT
+const swiper = new Swiper(".mySwiper", {
+    modules: [Navigation, Pagination],
+    spaceBetween: 45,
+    effect: "fade",
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
