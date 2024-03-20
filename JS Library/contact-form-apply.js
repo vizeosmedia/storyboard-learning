f0fd815effd198a71dc1804162aa8b003417c275
vizeosmedia <159869493+vizeosmedia@users.noreply.github.com>
2024-03-19 21:31:14 -0700

    Added Dropdown js init

--- JS Library/contact-form-apply.js
+++ JS Library/contact-form-apply.js
@@ -33,3 +33,39 @@ const swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+  // INSIGHTS DROPDOWN MENU
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+//loop through all dropdown elements
+dropdowns.forEach(dropdown => {
+const select = dropdown.querySelector('.select');
+const caret = dropdown.querySelector('.caret');
+const menu = dropdown.querySelector('.menu');
+const options = dropdown.querySelectorAll('.menu li');
+const selected = dropdown.querySelector('.selected');
+
+
+select.addEventListener('click', () => {
+console.log("hello");
+select.classList.toggle('select-clicked');
+caret.classList.toggle('caret-rotate');
+menu.classList.toggle('menu-open');
+});
+
+options.forEach(option => {
+option.addEventListener('click', () => {
+selected.innerText = option.innerText;
+select.classList.remove('select-clicked');
+caret.classList.remove('caret-rotate');
+menu.classList.remove('menu-open');
+options.forEach(option => {
+option.classList.remove('active');
+});
+option.classList.add('active');
+});
+});
+
+});
